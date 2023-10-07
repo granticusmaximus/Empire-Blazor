852eeda712bd96e727e76eb9566be5f42fd558c0
Grant Watson <grant@gwsapp.net>
2023-10-06 18:55:46 -0600

    Update to pages

---
+++ Empire/Service/AnalystService.cs
@@ -0,0 +1,30 @@
+﻿using Empire.Data;
+using Empire.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+
+namespace Empire.Service
+{
+    public class AnalystService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AnalystService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<Analyst[]> GetListOfAnalyst()
+        {
+            await _context.Analyst.ToListAsync()
+        }
+
+        public void Delete(int id)
+        {
+            var analyst = _dbContext.Analyst.SingleOrDefault(x => x.BAID == id);
+            _dbContext.Remove(analyst);
+            _dbContext.SaveChanges();
+        }
+    }
+}
