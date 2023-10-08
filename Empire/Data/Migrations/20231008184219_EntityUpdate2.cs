91f5606381f00ad9e27a37f4f88845b1a1c10c28
Grant Watson <grant@gwsapp.net>
2023-10-08 12:43:39 -0600

    Update to files

---
+++ Empire/Data/Migrations/20231008184219_EntityUpdate2.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Empire.Data.Migrations
+{
+    public partial class EntityUpdate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cellnumber",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Cellnumber",
+                table: "AspNetUsers");
+        }
+    }
+}
