330f67018e9b5ac5fd8373d3e0a5ee6b775150a0
Grant Watson <grant@gwsapp.net>
2023-10-11 17:25:00 -0600

    Missing semi colon

--- Empire/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Empire/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using Empire.Models
+using Empire.Models;
 
 namespace Empire.Areas.Identity.Pages.Account.Manage
 {
