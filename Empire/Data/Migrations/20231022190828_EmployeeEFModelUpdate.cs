d71d2b383713e8f1fc2eb345b94ec156aa7bb17b
Grant Watson <grant@gwsapp.net>
2023-10-22 14:21:36 -0600

    Employee Crud pages created, log in page update
    Added search feature on Employee list page

---
+++ Empire/Data/Migrations/20231022190828_EmployeeEFModelUpdate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Empire.Data.Migrations
+{
+    public partial class EmployeeEFModelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Employee",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Designation",
+                table: "Employee",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Employee",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "Designation",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Employee");
+        }
+    }
+}
