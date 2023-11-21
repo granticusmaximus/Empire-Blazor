cee35d1c4f34122c4f1abd514991fa592e9bd917
Grant Watson <grant@gwsapp.net>
2023-11-21 13:12:36 -0700

    Update to chat services

--- Empire/Service/ChatService.cs
+++ Empire/Service/ChatService.cs
@@ -1,6 +1,31 @@
-﻿namespace Empire.Service
+﻿using Empire.Models;
+
+namespace Empire.Service
 {
-    public class ChatService
+    public class ChatService : IChatService
     {
+        private readonly IMessagesPublisher _publisher;
+        private readonly IMessagesConsumer _consumer;
+
+        public ChatService(IMessagesPublisher publisher, IMessagesConsumer consumer)
+        {
+            _publisher = publisher;
+            _consumer = consumer;
+            _consumer.MessageReceived += OnMessage;
+        }
+
+        private void OnMessage(object sender, Message message)
+        {
+            this.MessageReceived?.Invoke(this, message);
+        }
+
+        public event EventHandler<Message> MessageReceived;
+
+        public IEnumerable<User> GetAllUsers() => _usersProvider.GetAll();
+
+        public async Task PostMessageAsync(User user, string message)
+        {
+            await _publisher.PublishAsync(new Message(user.Username, message, DateTime.UtcNow));
+        }
     }
 }
