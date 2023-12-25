535012959da5964079c035688772738007ce1966
Grant Watson <grant@gwsapp.net>
2023-12-24 19:00:45 -0700

    Changes

--- Empire/obj/Debug/net6.0/Empire.AssemblyInfo.cs
+++ Empire/obj/Debug/net6.0/Empire.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Empire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf0af618b9638633d73e3ea6b7b9f86d309cdd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Empire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Empire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
