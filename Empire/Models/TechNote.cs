60f3d5cd34d435d4e846568fe1bd3348353baf0c
Grant Watson <grant@gwsapp.net>
2024-01-31 12:45:21 -0500

    Update TechNote model and AssemblyInfo version

--- Empire/Models/TechNote.cs
+++ Empire/Models/TechNote.cs
@@ -7,9 +7,9 @@ namespace Empire.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Notes { get; set; }
-        public string Username { get; set; }
+        public string? Notes { get; set; }
+        public string? Username { get; set; }
 
-        public ICollection<TicketTechNote> TicketTechNotes { get; set; }
+        public ICollection<TicketTechNote>? TicketTechNotes { get; set; }
     }
 }
